64c4bb593a37f32dcee825057d82369f07ca300c
Kreysh1 <44417554+Kreysh1@users.noreply.github.com>
2021-09-23 16:11:11 -0600

    personalBlog Creation

--- blogPersonal/karma.conf.js
+++ blogPersonal/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Angular1'),
+      dir: require('path').join(__dirname, './coverage/blogPersonal'),
       subdir: '.',
       reporters: [
         { type: 'html' },
