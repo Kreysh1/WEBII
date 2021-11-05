7ca68e46ee4a187b28e5490c148f03a5667b314b
Kreysh1 <44417554+Kreysh1@users.noreply.github.com>
2021-11-05 11:54:40 -0700

    weather-app

--- issue-tracker/src/app/issues.service.ts
+++ issue-tracker/src/app/issues.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Issue } from './issue';
-import { issues } from 'src/assets/mock-issues';
+import { issues } from '../assets/mock-issues';
 
 @Injectable({
   providedIn: 'root'
