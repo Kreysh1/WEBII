37b5ee49e6773f2b242cf452824ff9e3542f03c5
Kreysh1 <44417554+Kreysh1@users.noreply.github.com>
2021-11-30 15:59:52 -0700

    Update Projects

--- my-editor/webpack.config.js
+++ my-editor/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /.ts$/,
+                test: /\.ts$/,
                 loader: 'ts-loader',
                 options: {
                     configFile: path.join(src, 'tsconfig.json')
