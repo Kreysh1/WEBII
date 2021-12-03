f3714c7d68d8a71538ac67f2d5ccdaaea7b6cf05
Kreysh1 <44417554+Kreysh1@users.noreply.github.com>
2021-12-03 14:30:51 -0700

    ui-controls final

--- my-components/src/app/app.component.ts
+++ my-components/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { Card } from 'ui-controls';
+import { assassins } from './assassins';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-components';
+  cards: Card[] = assassins;
+  onCardChange(cards: Card[]) {
+    console.log(cards);
+  }
+  log(){
+    alert(this.title + ' copied to the clipboard')
+  }
 }
+
--- my-components/src/app/app.module.ts
+++ my-components/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UiControlsModule } from 'ui-controls';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UiControlsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
